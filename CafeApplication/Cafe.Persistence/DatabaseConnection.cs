4aec4719bd50f3c4ee2f0c21d560957e74a243de
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-09-27 17:20:09 +0300

    Add database connection class

---
+++ CafeApplication/Cafe.Persistence/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace Cafe.Persistence
+{
+    public class DatabaseConnection
+    {
+        public static DatabaseConnection Instance { get => _instance; }
+        private static DatabaseConnection _instance;
+        private string? _connectionString;
+        private DatabaseConnection()
+        {
+            _connectionString = ConfigurationManager.AppSettings["cafedb"];
+        }
+
+        static DatabaseConnection()
+        {
+            _instance = new DatabaseConnection();
+        }
+
+        public string? GetConnectionString() => _connectionString;
+    }
+}
