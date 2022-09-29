e54cda28b1a22a4a9473067ca581593a75f53e38
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-09-29 11:17:38 +0300

    Add dbcontext interface

--- CafeApplication/Cafe.Application/Interfaces/ICafeDbContext.cs
+++ CafeApplication/Cafe.Application/Interfaces/ICafeDbContext.cs
@@ -1,9 +1,18 @@
-﻿
+﻿using Cafe.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cafe.Application.Interfaces
 {
-    public class ICafeDbContext
+    public interface ICafeDbContext
     {
-
+        DbSet<Dish> Dishes { get; set; }
+        DbSet<Employee> Employees { get; set; }
+        DbSet<Ingridient> Ingridients { get; set; }
+        DbSet<IngridientsDish> IngridientsDishes { get; set; }
+        DbSet<IngridientsWarehouse> IngridientsWarehouses { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<OrderDish> OrderDishes { get; set; }
+        DbSet<Profession> Professions { get; set; }
+        DbSet<Provider> Providers { get; set; }
     }
 }
--- CafeApplication/Cafe.Persistence/CafeContext.cs
+++ CafeApplication/Cafe.Persistence/CafeContext.cs
@@ -1,29 +1,29 @@
-﻿using Cafe.Domain;
+﻿using Cafe.Application.Interfaces;
+using Cafe.Domain;
 using Cafe.Persistence.EntityTypeConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cafe.Persistence
 {
-    public class CafeContext : DbContext
+    public class CafeContext : DbContext, ICafeDbContext
     {
         public CafeContext()
         {
         }
 
-        public CafeContext(DbContextOptions<CafeContext> options)
-            : base(options)
+        public CafeContext(DbContextOptions<CafeContext> options) : base(options)
         {
         }
 
-        public virtual DbSet<Dish> Dishes { get; set; } = null!;
-        public virtual DbSet<Employee> Employees { get; set; } = null!;
-        public virtual DbSet<Ingridient> Ingridients { get; set; } = null!;
-        public virtual DbSet<IngridientsDish> IngridientsDishes { get; set; } = null!;
-        public virtual DbSet<IngridientsWarehouse> IngridientsWarehouses { get; set; } = null!;
-        public virtual DbSet<Order> Orders { get; set; } = null!;
-        public virtual DbSet<OrderDish> OrderDishes { get; set; } = null!;
-        public virtual DbSet<Profession> Professions { get; set; } = null!;
-        public virtual DbSet<Provider> Providers { get; set; } = null!;
+        public DbSet<Dish> Dishes { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Ingridient> Ingridients { get; set; } = null!;
+        public DbSet<IngridientsDish> IngridientsDishes { get; set; } = null!;
+        public DbSet<IngridientsWarehouse> IngridientsWarehouses { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderDish> OrderDishes { get; set; } = null!;
+        public DbSet<Profession> Professions { get; set; } = null!;
+        public DbSet<Provider> Providers { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseSqlServer(DatabaseConnection.Instance.GetConnectionString());
