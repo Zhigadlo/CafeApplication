51b77b527ee762451dd94530f107c9d97745f6fd
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-11-16 16:45:16 +0300

    User system fixes

--- CafeApplication/Cafe.Web/Program.cs
+++ CafeApplication/Cafe.Web/Program.cs
@@ -1,5 +1,6 @@
 using Cafe.Application.Interfaces;
 using Cafe.Persistence;
+using Cafe.Web.Data;
 using Cafe.Web.Services;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
@@ -8,13 +9,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<CafeContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UserDbConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<CafeContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddTransient<ICafeDbContext, CafeContext>();
 builder.Services.AddTransient<IngridientService>();
